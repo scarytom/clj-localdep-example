60468a4fd2accbf832f951480b37b462d437803b
Tom Denley <tom.denley@youdevise.com>
2012-08-03 11:23:45 +0100

    add local jar

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 A Clojure library designed to ... well, that part is up to you.
 
+## Local Maven Repo
+Adding jars to the local maven repo in `vendor/mvn`
+
+```bash
+mvn --debug deploy:deploy-file -Dfile=/full/path/to/dummy.jar -DartifactId=dummy -Dversion=1.0.0 -DgroupId=com.scarytom.test -Dpackaging=jar -Durl=file:vendor/mvn
+```
+
 ## Usage
 
 FIXME
--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :repositories {"local" ~(str (.toURI (java.io.File. "vendor/mvn")))}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [com.scarytom.test/dummy "1.0.0"]])
